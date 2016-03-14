ac6e09a9a71e0740a500589fc96912b90ecf72a1
mikeadd <josephaddonisio@josephs-imac.cisco>
2016-03-13 22:57:57 -0400

    Removing unnecessary files
    removing depreciated files

--- MachineLearning.R
+++ MachineLearning.R
@@ -1,18 +1,20 @@
 library(readr)
+library(caret)
+library(rattle)
+library(doParallel)
 
-trainingurl = "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-training.csv"
-validationurl = "https://d396qusza40orc.cloudfront.net/predmachlearn/pml-testing.csv"
+trainingurl = "/Users/josephaddonisio/Downloads/Cousera/Machine Learning/pml-training.csv"
+validationurl = "/Users/josephaddonisio/Downloads/Cousera/Machine Learning/pml-testing.csv"
 
-train2 = read_csv(trainingurl, na = c("", "NA","#DIV/0!"))
-train = read.csv("C:\\Users\\EDMike\\Downloads\\pml-training.csv", na.strings=c("#DIV/0!", "NA","") )
-validation = read_csv(testingurl, na = c("", "NA","#DIV/0!"))
+train = read.csv(trainingurl,  na.strings=c("#DIV/0!", "NA","") )
+validation = read.csv(validationurl, na.strings = c("", "NA","#DIV/0!"))
 
 
 train.na = apply(train, 2, function (x) sum(is.na(x))/19622)
 NA.vars = which(train.na > .95)
-others.vars = which(names(train) %in% c('X', 'user_name', 'raw_timestamp_part_1', 'raw_timestamp_part_2', 'cvtd_timestamp', 'new_window', 'num_window'))
+other.vars = which(names(train) %in% c('X', 'user_name', 'raw_timestamp_part_1', 'raw_timestamp_part_2', 'cvtd_timestamp', 'new_window', 'num_window'))
 
-train = train[,-c(removed.vars2,other.vars)]
+train = train[,-c(NA.vars, other.vars)]
 
 table(train$classe)
 
@@ -24,4 +26,21 @@ testing <- train[-index,]
 tree.model = train(classe ~ .,method="rpart",data=training)
 fancyRpartPlot(tree.model$finalModel, sub = "")
 
-rf.model = train(classe ~ ., method = "rf", data = training )
+tree.prediction <- predict(tree.model, testing)
+confusionMatrix(tree.prediction, testing$classe)
+
+
+
+registerDoParallel()
+ctrl <- trainControl(classProbs=TRUE, savePredictions=TRUE, allowParallel=TRUE, number = 5)
+
+rf.model = train(classe ~ ., method = "rf", data = training, trControl = ctrl, importance = TRUE)
+
+rf.prediction <- predict(rf.model, testing)
+confusionMatrix(rf.prediction, testing$classe)
+
+var.imp = varImp(rf.model)
+var.imp
+plot(var.imp, top = 8)
+
+final.prediction <- predict(rf.model, validation, type = "class")
